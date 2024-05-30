a50a7362a631cbb7e2652e83cf7607b25b6d7c2d
mani <mani@email.com>
2024-05-30 18:03:38 +0530

    3.1

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -25,8 +25,8 @@ const ProductCard = ({ productDetails }: { productDetails: ProductDetails }): JS
     rating,
     availabilityStatus,
     thumbnail,
+    id
   } = productDetails;
-
 
   const dialogRef: RefObject<HTMLDialogElement> = useRef(null);
 
@@ -54,40 +54,33 @@ const ProductCard = ({ productDetails }: { productDetails: ProductDetails }): JS
 
 
   return (
-    <div className="relative cursor-pointer overflow-hidden rounded-md h-full min-h-96 w-full hover:shadow-md hover:shadow-green-200 shadow-custom text-gray-200 p-5" onClick={toggleDialog}>
+    <div className="relative group cursor-pointer overflow-hidden rounded-md h-full min-h-96 w-full hover:shadow-md hover:shadow-green-200 shadow-custom text-gray-200 p-5" onClick={toggleDialog}>
 
-      {/* product card start */}
-
-      <>
-        <div>
-          <Image loader={myLoader} src={thumbnail} alt={title} width={360} height={360} />
-        </div>
-        <div className="absolute left-0 -bottom-[420px] top-0 w-full border border-white flex flex-col justify-end items-start hover:bottom-0 hover:bg-black hover:opacity-50 hover:z-20 hover:text-white transition-all duration-500 p-3">
-          <p className="text-lg font-semibold mb-2 text-cyan-600">{title}</p>
-          <div className="">
-            <p className="mb-2 line-clamp-2">{description}</p>
-            <div className="p-3 bg-emerald-500 rounded-lg text-white flex flex-col justify-between items-between">
-              <div className="flex items-center justify-between">
-                <p className="text-sm text-gray-100">{availabilityStatus}</p>
-                <div className="flex items-center space-x-1">
-                  <span className="text-yellow-400 text-lg">{"\u2605"}</span>
-                  <p className="text-sm">{rating}</p>
-                </div>
+      <div>
+        <Image loader={myLoader} src={thumbnail} alt={title} width={360} height={360} className="group-hover:scale-150 transition-all duration-500" />
+      </div>
+      <div className="absolute left-0 -bottom-[100px] top-0 w-full bg-transparent flex flex-col justify-end items-start group-hover:bottom-0 group-hover:bg-black/30 group-hover:z-20 group-hover:text-white transition-all duration-500 p-3">
+        <p className="text-lg font-semibold mb-2 text-emerald-500 group-hover:text-white transition-colors duration-300">{title}</p>
+        <div className="">
+          <p className="mb-2 line-clamp-2 text-gray-600 group-hover:text-white">{description}</p>
+          <div className="p-3 bg-emerald-500 rounded-lg text-white flex flex-col justify-between items-between">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-100">{availabilityStatus}</p>
+              <div className="flex items-center space-x-1">
+                <span className="text-yellow-400 text-lg">{"\u2605"}</span>
+                <p className="text-sm">{rating}</p>
               </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <p className="text-sm">Save {discountPercentage}%</p>
-                  <p className="text-lg">${price}</p>
-                </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <p className="text-sm">Save {discountPercentage}%</p>
+                <p className="text-lg">${price}</p>
               </div>
             </div>
           </div>
         </div>
-      </>
+      </div>
 
-      {/* product card end */}
-
-      {/* dialog content */}
       <dialog ref={dialogRef} className="dialog w-[90%] h-[90%] bg-slate-100/20 rounded-lg">
         <div className="bg-white p-4 rounded-md flex flex-col">
           <button onClick={toggleDialog} className="bg-cyan-500 text-white py-1 px-3 rounded-md hover:bg-cyan-600 self-end mb-2">&#x2716;</button>
